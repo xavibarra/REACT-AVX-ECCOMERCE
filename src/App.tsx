50590306a042012f76705f0212a1cd261b0bf8b4
Xavier Barrachina <xbarrachinafont@gmail.com>
2024-05-27 17:59:59 +0200

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
-import { useEffect, useState } from "react";
-import { Category } from "./models/category";
+// import { useEffect, useState } from "react";
+// import { Category } from "./models/category";
 
 function App() {
-  const [categories, setCategories] = useState<Array<Category>>([]);
-  const [productCards, setProductCards] = useState<JSX.Element[]>([]);
+  //   const [categories, setCategories] = useState<Array<Category>>([]);
+  //   const [productCards, setProductCards] = useState<JSX.Element[]>([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  //   useEffect(() => {
+  //     fetchData();
+  //   }, []);
 
-  async function fetchData() {
-    const results = await (
-      await fetch("http://localhost:3000/categories")
-    ).json();
-    setCategories(results);
-  }
+  //   async function fetchData() {
+  //     const results = await (
+  //       await fetch("http://localhost:3000/categories")
+  //     ).json();
+  //     setCategories(results);
+  //   }
 
-  async function fetchCategory(id: string) {
-    const result = await (
-      await fetch(`http://localhost:3000/categories/${id}`)
-    ).json();
+  //   async function fetchCategory(id: string) {
+  //     const result = await (
+  //       await fetch(`http://localhost:3000/categories/${id}`)
+  //     ).json();
 
-    console.log(result);
-  }
+  //     console.log(result);
+  //   }
 
-  async function fetchProductsByCategory(id: string) {
-    try {
-      const results = await (
-        await fetch(`http://localhost:3000/products/productsByCategory/${id}`)
-      ).json();
+  //   async function fetchProductsByCategory(id: string) {
+  //     try {
+  //       const results = await (
+  //         await fetch(`http://localhost:3000/products/productsByCategory/${id}`)
+  //       ).json();
 
-      if (Array.isArray(results)) {
-        console.log(results);
-        const productCards = results.map((product) => (
-          <div key={product.id} className="product-card">
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="product-image"
-            />
-            <div className="product-details">
-              <h3 className="product-name">{product.name}</h3>
-              <p className="product-price">${product.price}</p>
-            </div>
-          </div>
-        ));
+  //       if (Array.isArray(results)) {
+  //         console.log(results);
+  //         const productCards = results.map((product) => (
+  //           <div key={product.id} className="product-card">
+  //             <img
+  //               src={product.imageUrl}
+  //               alt={product.name}
+  //               className="product-image"
+  //             />
+  //             <div className="product-details">
+  //               <h3 className="product-name">{product.name}</h3>
+  //               <p className="product-price">${product.price}</p>
+  //             </div>
+  //           </div>
+  //         ));
 
-        setProductCards(productCards);
-      } else {
-        console.log("No se encontraron productos para esta categoría");
-      }
-    } catch (error) {
-      console.error("Error al obtener los productos:", error);
-    }
-  }
+  //         setProductCards(productCards);
+  //       } else {
+  //         console.log("No se encontraron productos para esta categoría");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error al obtener los productos:", error);
+  //     }
+  //   }
 
   return (
     <>
-      <div className="categories-container">
+      {/* <div className="categories-container">
         {categories.map((category) => (
           <div
             className="category-item"
@@ -68,7 +68,7 @@ function App() {
           </div>
         ))}
       </div>
-      <div className="product-cards-container">{productCards}</div>
+      <div className="product-cards-container">{productCards}</div> */}
     </>
   );
 }
