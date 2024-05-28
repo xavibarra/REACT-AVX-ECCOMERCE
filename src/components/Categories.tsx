1326e3b77c4561072bd9fb24f528768415afd6b2
Xavier Barrachina <xbarrachinafont@gmail.com>
2024-05-28 11:33:53 +0200

    home

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,23 +1,24 @@
-import Cpu from "../assets/categories/cpu.svg";
 import Case from "../assets/categories/case.svg";
 import CaseFan from "../assets/categories/case_fan.svg";
+import Cpu from "../assets/categories/cpu.svg";
+import CpuCooler from "../assets/categories/cpu_cooler.svg";
 import External from "../assets/categories/external_hard_drive.svg";
-import Internal from "../assets/categories/internal_hard_drive.svg";
 import GraphicCard from "../assets/categories/graphic_card.svg";
+import Internal from "../assets/categories/internal_hard_drive.svg";
+import Logo from "../assets/categories/logoPortions.svg";
 import Monitor from "../assets/categories/monitor.svg";
+import Motherboard from "../assets/categories/motherboard.svg";
 import OpticalDrive from "../assets/categories/optical_drive.svg";
 import PowerSupply from "../assets/categories/power_supply.svg";
 import Ram from "../assets/categories/ram.svg";
-import CpuCooler from "../assets/categories/cpu_cooler.svg";
-import Logo from "../assets/categories/logoPortions.svg";
-import Motherboard from "../assets/categories/motherboard.svg";
-// import { ReactComponent as Cpu } from "../assets/categories/cpu.svg";
 
 const Categories = () => {
   return (
     <section id="components">
       <div className="container">
-        <div>{/* <svg src={Cpu} alt="CPU Icon" /> */}</div>
+        <div>
+          <img src={Cpu} alt="" />
+        </div>
         <div>
           <img src={Case} alt="CPU Icon" />
         </div>
@@ -51,9 +52,9 @@ const Categories = () => {
         <div>
           <img src={Motherboard} alt="CPU Icon" />
         </div>
-        <div className="center">
-          <p>ceneter</p>
-        </div>
+        <section className="categories-logo-container">
+          <img className="w-72" src={Logo} alt="" />
+        </section>
       </div>
     </section>
   );
