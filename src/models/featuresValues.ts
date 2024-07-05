d5629fc80a76fa24d1861d4e739613633f540326
Xavier Barrachina <xbarrachinafont@gmail.com>
2024-07-05 13:33:28 +0200

    features values

---
+++ src/models/featuresValues.ts
@@ -0,0 +1,6 @@
+export interface FeaturesValues {
+  id_product: string;
+  id_feature: string;
+  feature_name_es: string;
+  value: string;
+}
--- src/models/product.ts
+++ src/models/product.ts
@@ -2,7 +2,7 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  category_id: number;
+  categoryId: number;
   imageUrl: string;
   offer: boolean;
   discount: number;
