3b2ff258f8fa8c006c938c75badffa36e9c8621a
Àlex Virgili <alexvf12@gmail.com>
2024-07-10 13:33:06 +0200

    pagination

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../models/product";
@@ -6,6 +7,9 @@ import Loading from "../components/Loading";
 import Navbar2 from "../components/NavBar2";
 import Footer from "../components/Footer";
 import "../styles/category.css";
+import { FaCircleArrowRight, FaCircleArrowLeft } from "react-icons/fa6";
+
+const PAGE_SIZE = 10; // Número de productos por página
 
 const Category = () => {
   const { categoryId } = useParams<{ categoryId: string }>();
@@ -13,42 +17,46 @@ const Category = () => {
   const [categoryName, setCategoryName] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1); // Estado para el número total de páginas
   const [buttonBackExpanded, setButtonBackExpanded] = useState<boolean>(false);
-  const [backFilterColor, setBackFilterColor] = useState<string>("rgba(253, 174, 55, 0.2)");
+  const [backFilterColor, setBackFilterColor] = useState<string>(
+    "rgba(253, 174, 55, 0.2)"
+  );
+
+  // Función para cargar productos por categoría y página
+  const fetchProductsByCategory = async (page: number) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch(
+        `http://localhost:3000/products/productsByCategory/${categoryId}?page=${page}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al obtener los productos.");
+      }
+      const data: Product[] = await response.json();
+
+      // Calcular el número total de páginas
+      const totalCount = response.headers.get("X-Total-Count");
+      if (totalCount) {
+        const total = Math.ceil(Number(totalCount) / PAGE_SIZE);
+        setTotalPages(total);
+      }
+
+      // Actualizar estado con los nuevos productos cargados
+      setProducts(data);
+      setCurrentPage(page); // Actualizar la página actual
+    } catch (error) {
+      setError("Error al obtener los productos. Inténtelo de nuevo más tarde.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchProductsByCategory = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-
-        const response = await fetch(
-          `http://localhost:3000/products/productsByCategory/${categoryId}`
-        );
-
-        if (!response.ok) {
-          throw new Error("Error al obtener los productos.");
-        }
-        const data: Product[] = await response.json();
-
-        // Fetch category for each product sequentially
-        const productsWithCategory = await Promise.all(
-          data.map(async (product) => {
-            const category = await fetchCategory(product.categoryId);
-            return { ...product, category };
-          })
-        );
-
-        setProducts(productsWithCategory);
-      } catch (error) {
-        setError(
-          "Error al obtener los productos. Inténtelo de nuevo más tarde."
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
-
     const fetchCategoryName = async () => {
       try {
         const response = await fetch(
@@ -68,25 +76,28 @@ const Category = () => {
     };
 
     if (categoryId) {
-      fetchProductsByCategory();
+      fetchProductsByCategory(1); // Cargar la primera página inicialmente
       fetchCategoryName();
     }
   }, [categoryId]);
 
-  const fetchCategory = async (categoryId: number): Promise<Category> => {
-    const response = await fetch(
-      `http://localhost:3000/categories/${categoryId}`
-    );
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
-    }
-    const data: Category = await response.json();
-    return data;
-  };
-
   const handleFilterButtonClick = () => {
     setButtonBackExpanded((prev) => !prev);
-    setBackFilterColor(prevColor => prevColor === "rgba(253, 174, 55, 0.2)" ? "orange" : "rgba(253, 174, 55, 0.2)");
+    setBackFilterColor((prevColor) =>
+      prevColor === "rgba(253, 174, 55, 0.2)"
+        ? "orange"
+        : "rgba(253, 174, 55, 0.2)"
+    );
+  };
+
+  const handleLoadMore = () => {
+    fetchProductsByCategory(currentPage + 1); // Cargar la siguiente página
+  };
+
+  const handleLoadPrevious = () => {
+    if (currentPage > 1) {
+      fetchProductsByCategory(currentPage - 1); // Cargar la página anterior
+    }
   };
 
   if (loading) return <Loading />;
@@ -96,13 +107,26 @@ const Category = () => {
     <div>
       <div id="filter">
         <div className="backFilterContainer">
-          <div className={`buttonBack ${buttonBackExpanded ? 'expanded' : ''}`} style={{ backgroundColor: backFilterColor }}></div>
+          <div
+            className={`buttonBack ${buttonBackExpanded ? "expanded" : ""}`}
+            style={{ backgroundColor: backFilterColor }}></div>
         </div>
-        <button className={`buttonFilter ${buttonBackExpanded ? 'expanded' : ''}`} onClick={handleFilterButtonClick}></button>
+        <button
+          className={`buttonFilter ${buttonBackExpanded ? "expanded" : ""}`}
+          onClick={handleFilterButtonClick}></button>
         {buttonBackExpanded && <div className="blueDiv"></div>}
       </div>
       <Navbar2 />
-      <RepeatedTitle text="GRAPHIC CARDS" />
+      <div className="titleContainer">
+        {/* Mostrar solo los títulos de la primera página */}
+        {products.slice(0, PAGE_SIZE).map((product) => (
+          <RepeatedTitle
+            key={product.id}
+            text={product.categories.category_name_en}
+          />
+        ))}
+      </div>
+
       <div className="filterOrder">
         <button>Lowest price</button>
         <button>Highest price</button>
@@ -111,9 +135,26 @@ const Category = () => {
         <button>Name</button>
       </div>
       <div className="categoryProducts">
+        {/* Mostrar productos de la página actual */}
         {products.map((product) => (
           <FlipCard key={product.id} product={product} />
         ))}
+      </div>
+      {/* Información de paginación */}
+      <div className="pagination">
+        <p>
+          Página {currentPage} de {totalPages}
+        </p>
+        {currentPage > 1 && (
+          <button onClick={handleLoadPrevious}>
+            <FaCircleArrowLeft />
+          </button>
+        )}
+        {products.length >= PAGE_SIZE && (
+          <button onClick={handleLoadMore}>
+            <FaCircleArrowRight />
+          </button>
+        )}
       </div>
       <Footer />
     </div>
