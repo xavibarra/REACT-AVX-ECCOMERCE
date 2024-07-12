ab855d3561a2afe734e9e21860825f8510363dae
Àlex Virgili <alexvf12@gmail.com>
2024-07-12 12:41:59 +0200

    NOSE

--- src/models/category.ts
+++
@@ -1,9 +0,0 @@
-export interface Category {
-  id: string;
-  category_name_es: string;
-  category_name_en: string;
-  category_name_ca: string;
-  category_description_es: string;
-  category_description_ca: string;
-  category_description_en: string;
-}
