8d7c1276f77d9642cc4528469811c531e83b845c
Àlex Virgili <alexvf12@gmail.com>
2024-07-12 12:42:42 +0200

    KO

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App: React.FC = () => {
-  return <></>;
-};
-
-export default App;
--- src/models/featuresValues.ts
+++
@@ -1,6 +0,0 @@
-export interface FeaturesValues {
-  id_product: string;
-  id_feature: string;
-  feature_name_es: string;
-  value: string;
-}
