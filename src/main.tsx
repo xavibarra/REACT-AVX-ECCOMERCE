f42c00f860288b5e88eca747349339231137f516
Àlex Virgili <alexvf12@gmail.com>
2024-07-16 23:10:36 +0200

    admin start

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Profile from "./pages/Profile";
 import ProjectInfo from "./pages/ProjectInfo";
+import Admin from "./pages/Admin";
 
 const router = createBrowserRouter([
   {
@@ -55,6 +56,10 @@ const router = createBrowserRouter([
   {
     path: "likes",
     element: <Likes />,
+  },
+  {
+    path: "admin",
+    element: <Admin />,
   },
 ]);
 
